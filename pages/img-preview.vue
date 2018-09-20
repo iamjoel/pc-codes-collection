6822c20bb8e5831910a0d54fbe6885bd3dc91f31
joel <iamjoel007@gmail.com>
2018-09-20 17:24:20 +0800

    add img preview

---
+++ pages/img-preview.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main">
+    <img class="img" src="http://via.placeholder.com/200x100" alt="" ref="img" @click="preview('img')"/>
+  </div>
+</template>
+
+<script>
+var SmartPhoto
+export default {
+  data() {
+    return {
+
+    }  
+  },
+  methods: {
+    preview(ref) {
+      if(SmartPhoto) {
+        new SmartPhoto(".img")
+      } else {
+        import('smartphoto').then(module => {
+          var SmartPhoto = module
+          new SmartPhoto(".img")
+        })
+      }
+      
+    }
+  }
+}
+</script>
+<!-- 加css 有问题 -->
+<!-- <style src="@/assets/vendor/smartphoto.min.css"></style> -->
+<style scoped></style>
